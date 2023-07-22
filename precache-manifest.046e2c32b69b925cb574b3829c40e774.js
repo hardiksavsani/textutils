a815acbeaaf176c9401b468630250555c8cde857
Hardik Savsani <112060603+hardiksavsani@users.noreply.github.com>
2023-07-22 15:08:17 +0300

    Updates

---
+++ precache-manifest.046e2c32b69b925cb574b3829c40e774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bf1f262bc811ec76c91",
+    "url": "/textutils/static/css/main.e2624b69.chunk.css"
+  },
+  {
+    "revision": "5bf1f262bc811ec76c91",
+    "url": "/textutils/static/js/main.5bf1f262.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/textutils/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "b5c4c38989fe644778fc",
+    "url": "/textutils/static/js/2.b5c4c389.chunk.js"
+  },
+  {
+    "revision": "0e1bde7eb6c19e784b52",
+    "url": "/textutils/static/js/runtime~main.0e1bde7e.js"
+  },
+  {
+    "revision": "3b9e3751f86b4170077d76ee999cf75f",
+    "url": "/textutils/index.html"
+  }
+];
